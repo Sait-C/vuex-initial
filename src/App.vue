461480443391b5f23ae9a254500efe33152c6454
Muharrem Sait ÇOKTAŞ <coktasmuharremsait@gmail.com>
2023-01-27 21:28:38 +0300

    Updating State with Mutations

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,18 @@
   <h1>
     {{ store.state.count }}
   </h1>
+  <button @click="click">Increment</button>
 </template>
 
 <script setup>
 import { useStore } from 'vuex';
 
 const store = useStore();
+
+const click = () => {
+  // commit the mutation
+  store.commit('increment', 10);
+}
 
 console.log(store);
 </script>
--- src/store.js
+++ src/store.js
@@ -3,7 +3,15 @@ import { createStore } from 'vuex';
 export const store = createStore({
     state() { // it is like data() {return{}}
         return {
-            count: 0
+            count: 0 // it is going to be reactive
+        }
+    },
+
+    // This is where all your methods go that are going to update the state.
+    mutations: {
+        increment(state, payload) {
+            //state.count += 1;
+            state.count += payload;
         }
     }
 })
